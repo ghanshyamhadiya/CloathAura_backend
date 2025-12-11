84981d9777d9ff1e2712946cb2a2b5d5a80a98d1
Ghanshyam hadiya <160567608+ghanshyamhadiya@users.noreply.github.com>
2025-12-11 16:15:48 +0530

    Change SMTP transport settings for email sending

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
